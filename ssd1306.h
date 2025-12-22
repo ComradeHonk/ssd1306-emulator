7d3ea50afc7b48fdf5db7bb1a3b525974bd7dafb
ComradeHonk <bozydar427050@gmail.com>
2025-12-22 19:35:52 +0100

    Change wording

--- ssd1306.h
+++ ssd1306.h
@@ -1,5 +1,5 @@
 /**
- * This Library was based on SSD1306 OLED display library written
+ * This Library is based on SSD1306 OLED display library written
  * by Aleksander Alekseev (afiskon).
  *
  * https://github.com/afiskon/stm32-ssd1306
