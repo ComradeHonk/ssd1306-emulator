12a2e95196752d7f61ac8c340dd79492338792a4
ComradeHonk <bozydar427050@gmail.com>
2025-12-28 03:23:16 +0100

    style: shorten and simplfy renderer code

--- experimental.c
+++ experimental.c
@@ -1,10 +1,13 @@
 #include "experimental.h"
 #include "ssd1306.h"
+#include "ssd1306_conf.h"
 #include "stdint.h"
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 
+#ifdef EXP_OCTANT_RENDERER
 static const char* octant_table[256] = {
     " ",    "𜺨", "𜺫", "🮂",    "𜴀", "▘",    "𜴁", "𜴂", "𜴃", "𜴄", "▝",    "𜴅",
     "𜴆", "𜴇", "𜴈", "▀",    "𜴉", "𜴊", "𜴋", "𜴌", "🯦", "𜴍", "𜴎", "𜴏",
@@ -33,7 +36,7 @@ static const char* octant_table[256] = {
 void octantrenderer_UpdateScreen(
     uint8_t SSD1306_Buffer[SSD1306_BUFFER_SIZE], uint8_t SSD1306_TerminalBuffer[SSD1306_BUFFER_SIZE]
 ) {
-  uint16_t x, y;
+  uint16_t x, y, i, j;
 
   static bool draw_start = true;
   if (draw_start) {
@@ -53,82 +56,39 @@ void octantrenderer_UpdateScreen(
 
     for (x = 0; x < SSD1306_WIDTH - 1; x += 2) {
       // Get 2x4 area of pixels
-      size_t pixel_1_buffer_index = x + (y / 8) * SSD1306_WIDTH;
-      size_t pixel_2_buffer_index = (x + 1) + (y / 8) * SSD1306_WIDTH;
-      size_t pixel_3_buffer_index = x + ((y + 1) / 8) * SSD1306_WIDTH;
-      size_t pixel_4_buffer_index = (x + 1) + ((y + 1) / 8) * SSD1306_WIDTH;
-      size_t pixel_5_buffer_index = x + ((y + 2) / 8) * SSD1306_WIDTH;
-      size_t pixel_6_buffer_index = (x + 1) + ((y + 2) / 8) * SSD1306_WIDTH;
-      size_t pixel_7_buffer_index = x + ((y + 3) / 8) * SSD1306_WIDTH;
-      size_t pixel_8_buffer_index = (x + 1) + ((y + 3) / 8) * SSD1306_WIDTH;
+      size_t pixel_buffer_indices[4][2];
+      uint8_t pixel_offsets[4];
 
-      uint8_t row_1_offset = 1 << (y % 8);
-      uint8_t row_2_offset = 1 << ((y + 1) % 8);
-      uint8_t row_3_offset = 1 << ((y + 2) % 8);
-      uint8_t row_4_offset = 1 << ((y + 3) % 8);
+      for (i = 0; i < 4; i++) {
+        for (j = 0; j < 2; j++)
+          pixel_buffer_indices[i][j] = (x + j) + ((y + i) / 8) * SSD1306_WIDTH;
+        pixel_offsets[i] = 1 << ((y + i) % 8);
+      }
 
       // If all pixels haven't changed, do not re-render them
-      if (!draw_start &&
-          !((SSD1306_Buffer[pixel_1_buffer_index] ^ SSD1306_TerminalBuffer[pixel_1_buffer_index]) &
-            row_1_offset) &&
-          !((SSD1306_Buffer[pixel_2_buffer_index] ^ SSD1306_TerminalBuffer[pixel_2_buffer_index]) &
-            row_1_offset) &&
-          !((SSD1306_Buffer[pixel_3_buffer_index] ^ SSD1306_TerminalBuffer[pixel_3_buffer_index]) &
-            row_2_offset) &&
-          !((SSD1306_Buffer[pixel_4_buffer_index] ^ SSD1306_TerminalBuffer[pixel_4_buffer_index]) &
-            row_2_offset) &&
-          !((SSD1306_Buffer[pixel_5_buffer_index] ^ SSD1306_TerminalBuffer[pixel_5_buffer_index]) &
-            row_3_offset) &&
-          !((SSD1306_Buffer[pixel_6_buffer_index] ^ SSD1306_TerminalBuffer[pixel_6_buffer_index]) &
-            row_3_offset) &&
-          !((SSD1306_Buffer[pixel_7_buffer_index] ^ SSD1306_TerminalBuffer[pixel_7_buffer_index]) &
-            row_4_offset) &&
-          !((SSD1306_Buffer[pixel_8_buffer_index] ^ SSD1306_TerminalBuffer[pixel_8_buffer_index]) &
-            row_4_offset)) {
+      bool pixel_changed = false;
+
+      for (i = 0; !pixel_changed && i < 4; i++)
+        for (j = 0; !pixel_changed && j < 2; j++)
+          if ((SSD1306_Buffer[pixel_buffer_indices[i][j]] ^
+               SSD1306_TerminalBuffer[pixel_buffer_indices[i][j]]) &
+              pixel_offsets[i])
+            pixel_changed = true;
+
+      if (!draw_start && !pixel_changed) {
         move_cursor = true;
         continue;
       }
 
       // Synchronize buffers
-      if (SSD1306_Buffer[pixel_1_buffer_index] & row_1_offset)
-        SSD1306_TerminalBuffer[pixel_1_buffer_index] |= row_1_offset;
-      else
-        SSD1306_TerminalBuffer[pixel_1_buffer_index] &= ~row_1_offset;
-
-      if (SSD1306_Buffer[pixel_2_buffer_index] & row_1_offset)
-        SSD1306_TerminalBuffer[pixel_2_buffer_index] |= row_1_offset;
-      else
-        SSD1306_TerminalBuffer[pixel_2_buffer_index] &= ~row_1_offset;
-
-      if (SSD1306_Buffer[pixel_3_buffer_index] & row_2_offset)
-        SSD1306_TerminalBuffer[pixel_3_buffer_index] |= row_2_offset;
-      else
-        SSD1306_TerminalBuffer[pixel_3_buffer_index] &= ~row_2_offset;
-
-      if (SSD1306_Buffer[pixel_4_buffer_index] & row_2_offset)
-        SSD1306_TerminalBuffer[pixel_4_buffer_index] |= row_2_offset;
-      else
-        SSD1306_TerminalBuffer[pixel_4_buffer_index] &= ~row_2_offset;
-
-      if (SSD1306_Buffer[pixel_5_buffer_index] & row_3_offset)
-        SSD1306_TerminalBuffer[pixel_5_buffer_index] |= row_3_offset;
-      else
-        SSD1306_TerminalBuffer[pixel_5_buffer_index] &= ~row_3_offset;
-
-      if (SSD1306_Buffer[pixel_6_buffer_index] & row_3_offset)
-        SSD1306_TerminalBuffer[pixel_6_buffer_index] |= row_3_offset;
-      else
-        SSD1306_TerminalBuffer[pixel_6_buffer_index] &= ~row_3_offset;
-
-      if (SSD1306_Buffer[pixel_7_buffer_index] & row_4_offset)
-        SSD1306_TerminalBuffer[pixel_7_buffer_index] |= row_4_offset;
-      else
-        SSD1306_TerminalBuffer[pixel_7_buffer_index] &= ~row_4_offset;
-
-      if (SSD1306_Buffer[pixel_8_buffer_index] & row_4_offset)
-        SSD1306_TerminalBuffer[pixel_8_buffer_index] |= row_4_offset;
-      else
-        SSD1306_TerminalBuffer[pixel_8_buffer_index] &= ~row_4_offset;
+      for (i = 0; i < 4; i++) {
+        for (j = 0; j < 2; j++) {
+          if (SSD1306_Buffer[pixel_buffer_indices[i][j]] & pixel_offsets[i])
+            SSD1306_TerminalBuffer[pixel_buffer_indices[i][j]] |= pixel_offsets[i];
+          else
+            SSD1306_TerminalBuffer[pixel_buffer_indices[i][j]] &= ~pixel_offsets[i];
+        }
+      }
 
       if (move_cursor) {
         // Move terminal cursor to the position of the pixel batch using escape codes
@@ -139,22 +99,10 @@ void octantrenderer_UpdateScreen(
       // Calculate octant
       uint8_t octant_selector = 0x00;
 
-      if (SSD1306_Buffer[pixel_1_buffer_index] & row_1_offset)
-        octant_selector |= 1;
-      if (SSD1306_Buffer[pixel_2_buffer_index] & row_1_offset)
-        octant_selector |= 1 << 1;
-      if (SSD1306_Buffer[pixel_3_buffer_index] & row_2_offset)
-        octant_selector |= 1 << 2;
-      if (SSD1306_Buffer[pixel_4_buffer_index] & row_2_offset)
-        octant_selector |= 1 << 3;
-      if (SSD1306_Buffer[pixel_5_buffer_index] & row_3_offset)
-        octant_selector |= 1 << 4;
-      if (SSD1306_Buffer[pixel_6_buffer_index] & row_3_offset)
-        octant_selector |= 1 << 5;
-      if (SSD1306_Buffer[pixel_7_buffer_index] & row_4_offset)
-        octant_selector |= 1 << 6;
-      if (SSD1306_Buffer[pixel_8_buffer_index] & row_4_offset)
-        octant_selector |= 1 << 7;
+      for (i = 0; i < 4; i++)
+        for (j = 0; j < 2; j++)
+          if (SSD1306_Buffer[pixel_buffer_indices[i][j]] & pixel_offsets[i])
+            octant_selector |= 1 << (2 * i + j);
 
       printf("%s", octant_table[octant_selector]);
     }
@@ -179,3 +127,4 @@ void octantrenderer_UpdateScreen(
   // Move terminal cursor below the screen
   printf("\033[%d;%dH", SSD1306_HEIGHT / 4 + 3, 1);
 }
+#endif
